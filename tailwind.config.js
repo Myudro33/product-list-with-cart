3c65300e03b67fc31f476aa8817c125b1740ba6f
myudro33 <qanashvilinika7@gmail.com>
2024-08-27 18:05:01 +0400

    style: add custom colors in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {},
+    screens: {
+      xs: '375px',
+      lg: '1440px'
+    },
+    colors: {
+      red: 'hsl(14, 86%, 42%)',
+      green: 'hsl(159, 69%, 38%)',
+      'rose-50': ' hsl(20, 50%, 98%)',
+      'rose-100': 'hsl(13, 31%, 94%)',
+      'rose-300': ' hsl(14, 25%, 72%)',
+      'rose-500': 'hsl(12, 20%, 44%)',
+      'rose-900': 'hsl(14, 65%, 9%)'
+    }
   },
   plugins: []
 }
