14c82809be642eab175bd3a519c9d3c1624dfce5
myudro33 <qanashvilinika7@gmail.com>
2024-08-28 20:54:45 +0400

    feat: add cart component

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -5,7 +5,7 @@
       class="lg:w-40 lg:h-10 left-[25%] overflow-hidden top-[14.5rem] rounded-3xl bg-[#fff] font-medium border border-red absolute"
     >
       <div
-        @click="() => (cart = !cart)"
+        @click="openModal"
         class="w-full h-full flex justify-center items-center cursor-pointer"
         v-if="cart"
       >
@@ -13,9 +13,9 @@
         <h1 class="ml-2">Add to cart</h1>
       </div>
       <div class="w-full bg-red h-full flex justify-between items-center px-3" v-else>
-        <MinusIcon @click="decrement()" />
+        <MinusIcon @click="decrement" />
         {{ counter }}
-        <PlusIcon @click="increment()" />
+        <PlusIcon @click="increment" />
       </div>
     </div>
     <h1 class="font-thin text-rose-300 mt-5">{{ props.category }}</h1>
@@ -29,11 +29,18 @@ import { ref } from 'vue'
 import CartIcon from './icons/CartIcon.vue'
 import MinusIcon from './icons/MinusIcon.vue'
 import PlusIcon from './icons/PlusIcon.vue'
-
+import { useItemsStore } from '@/stores/items'
 const props = defineProps(['id', 'image', 'name', 'category', 'price'])
 const cart = ref(true)
 const counter = ref(1)
+const itemsStore = useItemsStore()
+const openModal = ()=>{
+  cart.value = !cart.value
+  itemsStore.increment(counter.value,props)
+}
 const decrement = () => {
+  itemsStore.decrement(counter.value,props,cart.value)
+
   if (counter.value < 2) {
     cart.value = true
     return
@@ -41,6 +48,7 @@ const decrement = () => {
   counter.value--
 }
 const increment = () => {
-  counter.value++
+counter.value++  
+itemsStore.increment(counter.value,props)
 }
 </script>
