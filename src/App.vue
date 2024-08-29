7219b4303cd2060414edd1fb80f44400fa4bf8aa
myudro33 <qanashvilinika7@gmail.com>
2024-08-29 18:00:27 +0400

    feat: add cart container

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import ItemContainer from './components/ItemContainer.vue'
+import CartContainer from './components/CartContainer.vue';
 </script>
 
 <template>
   <div class="flex xs:flex-col lg:flex-row w-full h-full lg:p-20">
     <ItemContainer/>
+    <CartContainer/>
   </div>
 </template>
