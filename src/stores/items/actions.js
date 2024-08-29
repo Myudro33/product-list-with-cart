b025872b62197177d111fbd3cd5e26322c400307
myudro33 <qanashvilinika7@gmail.com>
2024-08-29 20:04:46 +0400

    fix: change remove from cart function

--- src/stores/items/actions.js
+++ src/stores/items/actions.js
@@ -2,20 +2,27 @@ export default {
   removeFromCart(id) {
     const index = (element) => element.id == id
     const itemIndex = this.cart.findIndex(index)
+    const itemDataIndex = this.store.findIndex(index)
+    const newItemObject = { ...this.store[itemDataIndex], modal: false, quantity: 1 }
+    this.store[itemDataIndex] = newItemObject
     this.cart.splice(itemIndex, 1)
   },
   decrement(counter, props) {
-    const index = (element) => element.id == props.id
-    const itemIndex = this.cart.findIndex(index)
-    const itemDataIndex = this.store.findIndex(index)
-    const newObject = { ...this.cart[itemIndex], quantity: counter - 1 }
-    const newItemObject = { ...this.store[itemDataIndex], quantity: counter - 1 }
-    this.store[itemDataIndex] = newItemObject
-    this.cart[itemIndex] = newObject
-    if (counter - 1 === 0) {
+    if (counter !== 1) {
+      const index = (element) => element.id == props.id
+      const itemIndex = this.cart.findIndex(index)
+      const itemDataIndex = this.store.findIndex(index)
+      const newObject = { ...this.cart[itemIndex], quantity: counter - 1 }
+      const newItemObject = { ...this.store[itemDataIndex], quantity: counter - 1 }
+      this.store[itemDataIndex] = newItemObject
+      this.cart[itemIndex] = newObject
+    } else {
+      const index = (element) => element.id == props.id
+      const itemIndex = this.cart.findIndex(index)
+      const itemDataIndex = this.store.findIndex(index)
       this.cart.splice(itemIndex, 1)
       const newItemObject = { ...this.store[itemDataIndex], modal: false }
-      this.store[itemDataIndex] = newItemObject
+      return (this.store[itemDataIndex] = newItemObject)
     }
   },
   increment(counter, props) {
