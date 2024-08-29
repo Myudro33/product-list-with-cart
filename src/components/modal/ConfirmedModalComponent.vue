9e1e81602ac5a2952a5c8248ec005dd18d2aa42a
myudro33 <qanashvilinika7@gmail.com>
2024-08-29 22:58:16 +0400

    fiX: close modal on background click

--- src/components/modal/ConfirmedModalComponent.vue
+++ src/components/modal/ConfirmedModalComponent.vue
@@ -1,6 +1,6 @@
 <template>
-  <div  v-if="itemsStore.confirmModal" class="text-5xl absolute flex justify-center items-center top-0 w-screen bg-[#00000080] h-[100%]">
-    <div class="bg-[#fff] w-[30rem] h-auto rounded-lg p-10">
+  <div  @click="itemsStore.newOrder" v-if="itemsStore.confirmModal" class="text-5xl absolute flex justify-center items-center top-0 w-screen bg-[#00000080] h-[100%]">
+    <div @click.stop class="bg-[#fff] w-[30rem] h-auto rounded-lg p-10">
       <ConfirmedIcon/>
       <h1 class="text-[#000] text-4xl mt-2 font-bold">Order Confirmed</h1>
       <h1 class="text-xl text-rose-500 mt-2">We hope you enjoy your food!</h1>
@@ -9,7 +9,7 @@
         <h1 class="text-lg" >Order total</h1>
         <h1 class="text-xl font-bold">${{  parseFloat(itemsStore.total).toFixed(2) }}</h1>
     </div>
-    <button @click="itemsStore.newOrder" class="bg-red w-full h-10 text-lg rounded-3xl text-[#fff]">Start New Order</button>
+    <button @click="itemsStore.newOrder"  class="bg-red w-full h-10 text-lg rounded-3xl text-[#fff]">Start New Order</button>
     </div>
  </div>
 </template>
